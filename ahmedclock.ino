4b17f8f4c61e89321bd889373bc136b30d9e43b5
jake waters <jerkey@g-cipher.net>
2015-11-02 15:45:27 -0800

    documentation

--- ahmedclock.ino
+++ ahmedclock.ino
@@ -1,3 +1,10 @@
+// ahmedclock
+// drives one of the secondary windings on an AC-powered alarm clock
+// with 12VAC by switching a 12V battery polarity back and forth at 60Hz
+// using an Extruder Controller V2.2 board from an original makerbot.
+//
+// for a 2015 halloween costume appearing as Ahmed Mohammed
+// by http://github.com/jerkey
 #define DIR1    7
 #define DIR2    8
 #define ENABLE1 5
