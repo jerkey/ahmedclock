5006696644185175ea78cdc846911ae5485c4070
jake waters <jerkey@g-cipher.net>
2018-08-13 03:32:46 -0700

    first try at decoding s-bus frames

--- ahmedclock.ino
+++ ahmedclock.ino
@@ -1,27 +1,37 @@
-// ahmedclock
-// drives one of the secondary windings on an AC-powered alarm clock
-// with 12VAC by switching a 12V battery polarity back and forth at 60Hz
+// ahmedclock: branch dronearm
+// drives two DC motors on a drone, one which tightens a worm clamp onto a pole
+// the other which loosens to disengage the drone from the thing it just attached
+// to a pole.  for disaster.radio
 // using an Extruder Controller V2.2 board from an original makerbot.
 //
-// for a 2015 halloween costume appearing as Ahmed Mohammed
+// for a 2018 demonstration of drone mounting of a disaster.radio node
 // by http://github.com/jerkey
 #define DIR1    7
 #define DIR2    8
 #define ENABLE1 5
 #define ENABLE2 6
-#define HALFPERIOD 8 // milliseconds to wait
+
+// the atmega328 only has a single Serial port, and the default SBUS code
+// doesn't run on it. You can get it to work by not using the timer, but that
+// means you can't use delay in the loop() function anymore.
+
+#include <SBUS.h>
+SBUS sbus(Serial);
 
 void setup() {
+  sbus.begin(false); // we won't be able to use the delay() function
   pinMode(DIR1   ,OUTPUT);
   pinMode(DIR2   ,OUTPUT);
   pinMode(ENABLE1,OUTPUT);
   pinMode(ENABLE2,OUTPUT);
-  digitalWrite(ENABLE1,HIGH); // turn on output 1
+  digitalWrite(DIR1,HIGH);
+  digitalWrite(DIR2,HIGH);
 }
 
 void loop() {
-  digitalWrite(DIR1,HIGH);
-  delay(HALFPERIOD);
-  digitalWrite(DIR1,LOW);
-  delay(HALFPERIOD);
+  sbus.process();
+  int value = sbus.getChannel(1);
+  analogWrite(ENABLE1, value / 8);
+  value = sbus.getChannel(2);
+  analogWrite(ENABLE2, value / 8);
 }
